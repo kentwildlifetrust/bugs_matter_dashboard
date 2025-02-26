7b5753326809f7e647573dc93b6e93c376ff19be
kialuna <77881537+kialuna@users.noreply.github.com>
2025-02-26 16:11:24 +0000

    updated dockerfile and test div

--- app.R
+++ app.R
@@ -244,7 +244,9 @@ the windscreens of cars now compared to a decade or
 several decades ago. These observations, which have also 
 been reported from empirical data (Møller, 2019), have been 
 interpreted as an indicator of major global declines in insect 
-abundance. "))
+abundance. ")),
+          div("Invisible test",style="display:none;",id="test-div")
+          
       ),
       div(style = "padding: 20px; background-color: #C6E5E8; border-radius: 0px;",
           h3(style = "text-align: center; color: #0F0F0F;", "Global insect declines"),
